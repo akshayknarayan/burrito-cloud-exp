3bf1d88bb04257eaaf8bf5ecd2a77b4b5f666ed3
Akshay Narayan <akshayn@mit.edu>
2021-05-13 17:47:09 +0000

    Update

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use color_eyre::eyre::{ensure, eyre, Report, WrapErr};
 use openssh::Session;
 use std::path::{Path, PathBuf};
 use structopt::StructOpt;
-use tracing::{debug, info, instrument};
+use tracing::{debug, info, instrument, warn};
 use tracing_error::ErrorLayer;
 use tracing_subscriber::prelude::*;
 use tsunami::providers::{aws, azure, baremetal};
@@ -26,10 +26,12 @@ async fn with_launcher(
     launcher: &mut impl tsunami::Tsunami,
     machine_name: &str,
     script_remote_path: &Path,
+    bench_remote_path: &Path,
+    prov: &str,
 ) -> Result<(), Report> {
     let conns = launcher.connect_all().await?;
     let vm = conns.get(machine_name).unwrap();
-    do_exp(&vm.ssh, &script_remote_path).await
+    do_exp(&vm.ssh, script_remote_path, bench_remote_path, prov).await
 }
 
 #[derive(serde::Deserialize, serde::Serialize, Clone, Debug)]
@@ -48,24 +50,37 @@ impl Node {
         info!("starting machines");
 
         const MACHINE_NAME: &str = "burrito-test-machine";
+        let chmod_cmd = format!("chmod +x {}", bench_remote_path.to_str().unwrap());
         match self {
             Node::Aws { region } => {
                 let srp = script_remote_path.clone();
+                let brp = bench_remote_path.clone();
                 let mut aws_launcher = aws::Launcher::default();
                 aws_launcher.set_mode(aws::LaunchMode::TrySpot { hours: 6 });
+                let ami = ubuntu_ami::get_latest(
+                    &region,
+                    Some("focal"),
+                    None,
+                    Some("hvm:ebs-ssd"),
+                    Some("amd64"),
+                )
+                .await
+                .map_err(|e| eyre!(e))?;
                 let m = aws::Setup::default()
-                    .region_with_ubuntu_ami(region.clone().parse()?)
-                    .await?
+                    .region(region.clone().parse()?, ami, "ubuntu")
                     .instance_type("t3.medium")
                     .setup(move |vm| {
-                        info!(?vm, "wait");
-                        wait_for_continue();
-                        let bench_remote_path = bench_remote_path.clone();
+                        let bench_remote_path = brp.clone();
                         let bench_bin = bench_bin.clone();
+                        let script = script.clone();
                         let script_remote_path = srp.clone();
+                        let chmod_cmd = chmod_cmd.clone();
                         Box::pin(async move {
+                            install_deps(&vm.ssh).await?;
                             write_file(&vm.ssh, &bench_bin, bench_remote_path.as_path()).await?;
-                            write_file(&vm.ssh, &bench_bin, script_remote_path.as_path()).await?;
+                            let ok = vm.ssh.shell(&chmod_cmd).status().await?;
+                            ensure!(ok.success(), "chmod bench");
+                            write_file(&vm.ssh, &script, script_remote_path.as_path()).await?;
                             Ok(())
                         })
                     });
@@ -80,23 +95,39 @@ impl Node {
                     return Err(e);
                 }
 
-                let res = with_launcher(&mut aws_launcher, MACHINE_NAME, &script_remote_path).await;
+                //wait_for_continue();
+
+                let res = with_launcher(
+                    &mut aws_launcher,
+                    MACHINE_NAME,
+                    &script_remote_path,
+                    bench_remote_path.as_path(),
+                    "aws",
+                )
+                .await;
                 aws_launcher.terminate_all().await?;
                 res
             }
             Node::Azure { region: r } => {
                 let srp = script_remote_path.clone();
+                let brp = bench_remote_path.clone();
                 let mut az_launcher = azure::Launcher::default();
                 let m = azure::Setup::default()
                     .region(r.clone().parse()?)
+                    .image("Canonical:0001-com-ubuntu-server-focal:20_04-lts:latest".to_owned())
                     .instance_type("Standard_B2ms".to_owned())
                     .setup(move |vm| {
-                        let bench_remote_path = bench_remote_path.clone();
+                        let bench_remote_path = brp.clone();
                         let bench_bin = bench_bin.clone();
+                        let script = script.clone();
                         let script_remote_path = srp.clone();
+                        let chmod_cmd = chmod_cmd.clone();
                         Box::pin(async move {
+                            install_deps(&vm.ssh).await?;
                             write_file(&vm.ssh, &bench_bin, bench_remote_path.as_path()).await?;
-                            write_file(&vm.ssh, &bench_bin, script_remote_path.as_path()).await?;
+                            let ok = vm.ssh.shell(&chmod_cmd).status().await?;
+                            ensure!(ok.success(), "chmod bench");
+                            write_file(&vm.ssh, &script, script_remote_path.as_path()).await?;
                             Ok(())
                         })
                     });
@@ -108,24 +139,36 @@ impl Node {
                     return Err(e);
                 }
 
-                let res = with_launcher(&mut az_launcher, MACHINE_NAME, &script_remote_path).await;
+                let res = with_launcher(
+                    &mut az_launcher,
+                    MACHINE_NAME,
+                    &script_remote_path,
+                    bench_remote_path.as_path(),
+                    "azure",
+                )
+                .await;
                 az_launcher.terminate_all().await?;
                 res
             }
             Node::Baremetal { ip: i, user: u } => {
                 let mut launcher = baremetal::Machine::default();
                 let srp = script_remote_path.clone();
+                let brp = bench_remote_path.clone();
                 let m =
                     tsunami::providers::baremetal::Setup::new((i.as_str(), 22), Some(u.clone()))?
                         .setup(move |vm| {
-                            let bench_remote_path = bench_remote_path.clone();
+                            let bench_remote_path = brp.clone();
                             let bench_bin = bench_bin.clone();
+                            let script = script.clone();
                             let script_remote_path = srp.clone();
+                            let chmod_cmd = chmod_cmd.clone();
                             Box::pin(async move {
+                                install_deps(&vm.ssh).await?;
                                 write_file(&vm.ssh, &bench_bin, bench_remote_path.as_path())
                                     .await?;
-                                write_file(&vm.ssh, &bench_bin, script_remote_path.as_path())
-                                    .await?;
+                                let ok = vm.ssh.shell(&chmod_cmd).status().await?;
+                                ensure!(ok.success(), "chmod bench");
+                                write_file(&vm.ssh, &script, script_remote_path.as_path()).await?;
                                 Ok(())
                             })
                         });
@@ -135,22 +178,74 @@ impl Node {
                     .await?;
                 let conns = launcher.connect_all().await?;
                 let vm = conns.get(MACHINE_NAME).unwrap();
-                do_exp(&vm.ssh, &script_remote_path).await
+                do_exp(
+                    &vm.ssh,
+                    &script_remote_path,
+                    bench_remote_path.as_path(),
+                    "gcp",
+                )
+                .await
             }
         }
     }
 }
 
-async fn do_exp(ssh: &Session, script_remote_path: &Path) -> Result<(), Report> {
-    info!(?ssh, "wait");
-    wait_for_continue();
-
+async fn do_exp(
+    ssh: &Session,
+    script_remote_path: &Path,
+    bench_remote_path: &Path,
+    prov: &str,
+) -> Result<(), Report> {
     let mut cmd = ssh.command("python3");
     cmd.arg(script_remote_path.to_str().unwrap());
+    cmd.arg(Path::new(".").join(bench_remote_path).to_str().unwrap());
+    cmd.arg(prov);
     info!(?cmd, "running");
-    let status = cmd.status().await?;
-    ensure!(status.success(), "script failed");
-    info!("getting files");
+
+    let out = cmd.output().await?;
+    if !out.status.success() {
+        warn!("script failed");
+        println!("{}", String::from_utf8(out.stderr).unwrap());
+    }
+
+    tokio::fs::write(format!("{}.log", prov), out.stdout).await?;
+
+    info!("done, getting files");
+    let mut sftp = ssh.sftp();
+
+    let inter_req_times = [0, 25, 50, 75, 100];
+    //let num_receivers = [1, 2, 5, 10];
+    let num_receivers = [1];
+    let num_groups = [0, 1, 2, 5, 10]; // + be
+    let mut fnames = vec![];
+    for inter_req in &inter_req_times[..] {
+        for rcvrs in &num_receivers[..] {
+            fnames.push(format!(
+                "exp-{}-be-{}ms-{}rcvrs-client.data",
+                prov, inter_req, rcvrs
+            ));
+            for grps in &num_groups[..] {
+                fnames.push(format!(
+                    "exp-{}-ord:{}g-{}ms-{}rcvrs-client.data",
+                    prov, grps, inter_req, rcvrs
+                ));
+            }
+        }
+    }
+    //let fnames = ["transition-25ms-aws-ord5g.data"];
+    for fname in &fnames[..] {
+        let f = sftp.read_from(&fname).await;
+        if let Err(err) = f {
+            warn!(?err, ?fname, "file error");
+        } else {
+            let mut f = f.unwrap();
+            let mut local = tokio::fs::File::create(&fname).await?;
+            tokio::io::copy(&mut f, &mut local).await?;
+            f.close().await?;
+        }
+    }
+
+    wait_for_continue();
     Ok(())
 }
 
@@ -208,4 +303,48 @@ fn wait_for_continue() {
     let stdin = std::io::stdin();
     let mut iterator = stdin.lock().lines();
     iterator.next().unwrap().unwrap();
+}
+
+async fn install_deps(ssh: &Session) -> Result<(), Report> {
+    apt_install(ssh).await?;
+    let st = ssh
+        .shell("sudo pip3 install agenda")
+        .status()
+        .await
+        .wrap_err("pip install")?;
+    ensure!(st.success(), "pip install");
+    Ok(())
+}
+
+async fn apt_install(ssh: &Session) -> Result<(), Report> {
+    let mut count = 0;
+    loop {
+        count += 1;
+        let res = async {
+            let status = ssh
+                .shell("sudo add-apt-repository -y ppa:redislabs/redis")
+                .status()
+                .await
+                .wrap_err("redis repository add failed")?;
+            ensure!(status.success(), "redis apt-add-repository failed");
+            let status = ssh.shell(
+                "sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt install -y python3-pip redis && sudo /etc/init.d/redis-server stop",
+            ).status().await.wrap_err("apt install failed")?;
+            ensure!(status.success(), "apt install failed");
+            Ok(())
+        }
+        .await;
+
+        if res.is_ok() {
+            return res;
+        } else {
+            warn!(?res, "apt failed");
+        }
+
+        if count > 15 {
+            return res;
+        }
+
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+    }
 }
